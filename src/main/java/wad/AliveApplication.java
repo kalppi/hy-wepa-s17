c42972981861147e52c56ccb156d70b46e813139
Jarno Luukkonen <luukkonen.jarno@gmail.com>
2017-11-26 22:01:33 +0200

    fix?

--- src/main/java/wad/AliveApplication.java
+++ src/main/java/wad/AliveApplication.java
@@ -1,10 +1,12 @@
 package wad;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @EnableJpaRepositories("wad.repository")
 public class AliveApplication {
 
