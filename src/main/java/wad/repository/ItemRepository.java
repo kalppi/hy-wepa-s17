152d4cb999a13b6ecdd981773da1146e1ff89ea7
Jarno Luukkonen <luukkonen.jarno@gmail.com>
2017-11-26 21:47:56 +0200

    fix?

--- src/main/java/wad/repository/ItemRepository.java
+++ src/main/java/wad/repository/ItemRepository.java
@@ -1,7 +1,9 @@
 package wad.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import wad.domain.Item;
 
+@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 }
